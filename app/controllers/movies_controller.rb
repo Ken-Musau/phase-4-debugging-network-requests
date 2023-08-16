9251f8c622f08572c7388be35bee933f0f80031e
Kennedy Musau <kennedy.musauu@gmail.com>
2023-08-16 21:26:01 +0300

    debug network requests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,24 @@
 class MoviesController < ApplicationController
-  
+  # wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
     movies = Movie.all
     render json: movies
   end
 
+  def create
+    render json: Movie.create(movie_params), status: :created
+    # byebug
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+  def render_not_found_response
+    render json: { error: "Movies not found" }
+  end
 end
